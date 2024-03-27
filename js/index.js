21da4acb49f2a3c52ac1a5e56814182b5b1c925e
Marco <zehow.zhw@gmail.com>
2024-03-27 12:58:04 +0100

    adding the banner img with txt and anims

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var links = document.querySelectorAll(".navbar-link a");
 
-    links.forEach(function(link) {
-        link.addEventListener("click", function() {
-            links.forEach(function(link) {
+    links.forEach(function (link) {
+        link.addEventListener("click", function () {
+            links.forEach(function (link) {
                 link.classList.remove("selected");
             });
 
@@ -15,25 +15,40 @@ document.addEventListener("DOMContentLoaded", function() {
     var navbarLinks = document.querySelector(".navbar-link");
     var burgerIcon = document.querySelector(".burger-icon");
 
-    burgerMenu.addEventListener("click", function(event) {
+    burgerMenu.addEventListener("click", function (event) {
         event.stopPropagation();
 
         navbarLinks.classList.toggle("active");
         burgerIcon.classList.toggle("active");
     });
 
-    burgerIcon.addEventListener("click", function(event) {
+    burgerIcon.addEventListener("click", function (event) {
         event.stopPropagation();
 
         navbarLinks.classList.toggle("active");
         burgerIcon.classList.toggle("active");
     });
 
-    navbarLinks.querySelectorAll("a").forEach(function(link) {
-        link.addEventListener("click", function() {
+    navbarLinks.querySelectorAll("a").forEach(function (link) {
+        link.addEventListener("click", function () {
             burgerMenu.classList.remove("active");
             navbarLinks.classList.remove("active");
             burgerIcon.classList.remove("active");
         });
     });
 });
+
+/* Banner Image Accueil */
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
